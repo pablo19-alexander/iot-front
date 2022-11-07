6e0743a5bff19cb4756eda1d5919f1a3855ddac9
pablo19-alexander <pablo.pass1905@gmai.com>
2022-11-07 15:25:55 -0500

    plantilla instalada

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './css/App.css';
+import React from 'react'
+import Aside from './components/Aside'
+import Content from './components/Content'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header/>
+      <Aside/>
+      <Content/>
+      <Footer/>
+    </>
+  )
 }
-
-export default App;
