1212b8a24870c4a365d4e2f0ca524970193db4c7
pablo19-alexander <pablo.pass1905@gmai.com>
2022-11-25 20:59:40 -0500

    entorno de driver

--- src/components/cruds/DriverCrud.jsx
+++ src/components/cruds/DriverCrud.jsx
@@ -2,18 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class DriverCrud extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    
-    render() { 
-        return ( 
-            <>
-                <h1>contenido del crud de DriverCrud</h1>
-            </>
-         );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="content-wrapper">
+        <section className="content-header">
+          <div className="container-fluid">
+            <div className="row mb-2">
+              <div className="col-sm-6">
+                <h1>
+                  CRUD <strong>Driver</strong>
+                </h1>
+              </div>
+            </div>
+          </div>
+          {/* /.container-fluid */}
+        </section>
+        {/* crud de driver  */}
+        
+      </div>
+    );
+  }
 }
- 
+
 export default DriverCrud;
