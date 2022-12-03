3bd46bf2953728d589cf3e3349a66f26b7776302
pablo19-alexander <pablo.pass1905@gmai.com>
2022-12-03 18:29:51 -0500

    se arreglo el svg de los logos del crud

--- src/components/cruds/UserCrud/UserList.jsx
+++ src/components/cruds/UserCrud/UserList.jsx
@@ -184,7 +184,7 @@ class UserList extends React.Component {
                                     <td>
                                       <div className="btn-group" role="group" arial-label="">
                                         <Link className="" to={"/editar"}>
-                                          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-edit" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-edit" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#00abfb" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                                             <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
                                             <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
@@ -193,7 +193,7 @@ class UserList extends React.Component {
                                         </Link>
                                         <div className="mx-1"></div>
                                         <Link type="button" className="me-4">
-                                          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ff2825" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                                             <line x1="4" y1="7" x2="20" y2="7" />
                                             <line x1="10" y1="11" x2="10" y2="17" />
